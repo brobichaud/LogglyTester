37e684c2853bad7ba8acc32677a7e8069e2c44f3
Brett Robichaud <brobichaud@digimarc.com>
2017-12-22 15:47:35 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Digimarc.Shared;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw = new Stopwatch();
+			for (int x = 1; x < int.MaxValue; x++)
+			{
+				sw.Restart();
+				Logger.Error("This is a test of Loggly");
+				sw.Stop();
+				Console.WriteLine("Event #{0} took: {1}", x, sw.Elapsed);
+			}
+		}
+
+		public static string error =
+			"{" +
+			"\"time\" : \"2012-10-03T19:18:51.839\"," +
+			"\"source\" : \"webportal\"," +
+			"\"deployment\" : \"live\", " +
+			"\"logger\" : \"Digimarc.Apps.WebPortal.resolverData.ProcessRequest\"," +
+			"\"level\" : \"ERROR\"," +
+			"\"thread\" : \"13\"," +
+			"\"event\" : \"System.Exception: This is a test of Loggly\"" +
+			"}";
+	}
+}
